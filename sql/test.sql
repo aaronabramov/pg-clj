41492ad5a4b447216f0cb23ceb711e61190605a5
Aaron Rutkovsky <aaron.rutkovsky@gmail.com>
2013-09-14 22:27:46 -0500

    test query

--- sql/test.sql
+++ sql/test.sql
@@ -1,1 +1,8 @@
-SELECT (1);
+create table test_table (
+  key serial,
+  test1 char(4),
+  test2 varchar(32),
+  test3 varchar(64) not null
+);
+
+select column_name from INFORMATION_SCHEMA.COLUMNS where table_name = 'test_table';
