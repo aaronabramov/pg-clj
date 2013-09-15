9acebc14370e1721646e81c4eef156c8ea98404d
Aaron Rutkovsky <aaron.rutkovsky@gmail.com>
2013-09-14 22:18:46 -0500

    Execute PG queries

--- src/pg_clj/core.clj
+++ src/pg_clj/core.clj
@@ -1,5 +1,20 @@
-(ns pg-clj.core)
+(ns pg-clj.core
+  (:require [clojure.java.jdbc :as j]
+            [clojure.string :refer [split trim]]))
+
+(def db "jdbc:postgresql://localhost:5432/pg_clj")
+
+(defn print-query-results [query]
+  (println "\033[32mExecuting query:\033[0m")
+  (println (str "\033[36m" query "\033[0m"))
+  (j/with-connection db
+    (j/with-query-results rs [query]
+      (doseq [row rs]
+        (println row)))))
+
 
 (defn -main [query-name]
-  (let [query (slurp (str "./sql/" query-name ".sql"))]
-    (println query)))
+  (let [queries-string (trim (slurp (str "./sql/" query-name ".sql")))
+        queries (split queries-string #";")]
+    (doseq [query queries]
+      (print-query-results query))))
